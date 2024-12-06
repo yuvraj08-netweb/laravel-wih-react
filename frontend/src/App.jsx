caae48b66369b66d1e1106252a811722c94c94f1
Akshat Bhatya <akshatbhatya@gmail.com>
2024-12-06 12:30:04 +0530

    add header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { Outlet } from "react-router-dom";
-
+import Header from "./Components/Header/Header";
 function App() {
   return (
     <>
+      <Header />
       <Outlet />
     </>
   );
---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>home</li>
+          <li>home</li>
+          <li>home</li>
+          <li>home</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Header;
