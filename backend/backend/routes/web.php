67224a0a6d269f0d063c808f4a6651b3f947e2ea
akshat <akshatbhatya@gmail.com>
2024-12-15 21:51:44 +0530

    add cors and re install laravel

--- backend/backend/routes/web.php
+++ backend/backend/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthControlller;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::get("/signup",[AuthControlller::class,'signUpController']);
+Route::post('/signup', [AuthController::class, 'signUpController']);
